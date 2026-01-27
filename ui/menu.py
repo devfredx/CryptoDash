036b090689679dfc9297beea677ee6d66c91c569
devfredx <dev.fredx@gmail.com>
2026-01-27 23:01:41 +0300

    refactor: translate Menu class methods and comments to English

--- ui/menu.py
+++ ui/menu.py
@@ -1,26 +1,32 @@
 import os
 from ui.strings import STRINGS
 
+
 class Menu:
     @staticmethod
-    def temizle():
+    def clear_screen():
+        """Clears the terminal screen."""
         os.system('cls' if os.name == 'nt' else 'clear')
 
     @staticmethod
-    def misafir_paneli_ciz(dil):
-        s = STRINGS[dil]
-        Menu.temizle()
-        print("=" * 72)
+    def draw_guest_dashboard(language):
+        """Renders the dashboard for unauthenticated users."""
+        s = STRINGS[language]
+        Menu.clear_screen()
+
+        # UI Frame
+        print("=" * 74)
         print(f"   {s['app_title']}")
-        print("=" * 72)
-        print(f" {s['menu_header_left']:<33} |  {s['menu_header_right']}")
-        print("-" * 34 + " | " + "-" * 35)
-        print(f" {s['m_markets']:<33} |  {s['m_login']}")
-        print(f" {s['m_news']:<33} |  {s['m_register']}")
-        print(f" {s['m_support']:<33} |  ")
-        print(f" {s['m_lang']:<33} |  {s['m_exit']}")
-        print("-" * 34 + " | " + "-" * 35)
+        print("=" * 74)
+        print(f" {s['menu_header_left']:<34} |  {s['menu_header_right']}")
+        print("-" * 35 + " | " + "-" * 36)
+        print(f" {s['m_markets']:<34} |  {s['m_login']}")
+        print(f" {s['m_news']:<34} |  {s['m_register']}")
+        print(f" {s['m_support']:<34} |  ")
+        print(f" {s['m_lang']:<34} |  {s['m_exit']}")
+        print("-" * 35 + " | " + "-" * 36)
 
     @staticmethod
-    def mesaj_goster(metin):
-        print(f"\n[!] {metin}")
+    def show_message(text):
+        """Displays a system message to the user."""
+        print(f"\n[!] {text}")

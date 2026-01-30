33b9be2caf5bf8c1d58cf1f2fa91f0426a98d1df
devfredx <dev.fredx@gmail.com>
2026-01-30 20:56:51 +0300

    feat: add history list to User model for transaction tracking

--- models/user.py
+++ models/user.py
@@ -3,7 +3,8 @@ class User:
         self.username = username
         self.password = password
         self.balance = balance
-        self.assets = {}  # Örn: {'BTC': 0.1, 'ETH': 2.0}
+        self.assets = {}  # Örn: {'BTC': 0.1}
+        self.history = [] # YENİ: İşlem geçmişi listesi
 
     def __str__(self):
         return f"User: {self.username} | Balance: {self.balance} USDT"
